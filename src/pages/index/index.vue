b9a8dfd8ef9f572daa701db548039a35b105d5e6
Saud <submit2mxh@gmail.com>
2022-06-02 12:49:02 +0800

    chenged  kuaishou date id

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -78,7 +78,7 @@ export default {
           Message.success('复制成功')
           document.body.removeChild(inputEl)
           _ks_trace.push({
-            event: 'form', convertId: 442890, cb: function () {
+            event: 'form', convertId: 445832, cb: function () {
               console.log('Your callback function here!')
             }
           })
